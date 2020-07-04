2772548aa7d2c4039086da24a6675522c3ac70fa
kyle103507 <61506510+kyle103507@users.noreply.github.com>
2020-07-04 21:03:45 +0800

    類別跟方法．靜態API

---
+++ unity-run/Assets/Scripes/Exercise_1.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Exercise_1 : MonoBehaviour
+{
+    private void Start()
+    {
+        print(Mathf.PI);
+        //0-1隨機數值
+        print(Random.value);
+        print(Mathf.Abs(999999999999999999));
+        print(Random.Range(200, 500+1));
+    }
+    private void Update()
+    {
+       // print(Time.time);
+    }
+
+}
---
+++ unity-run/Assets/Scripes/Player.cs
@@ -0,0 +1,76 @@
+﻿
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    #region 人物控制
+
+    [Header("速度"),Tooltip("角色的移動速度")]
+    public int speed=50;
+    [Header("血量"),Tooltip("角色的血量呢"),Range(100.0f, 1500.0f)]
+    public float hp=99.9f;
+    public int coin;
+    public float Jumpheight=500;
+    [Header("音效區域")]
+    public AudioClip audoJump;
+    public AudioClip audoslide;
+    public AudioClip audohit;
+    [Header("角色死亡")]
+    public bool death;
+
+    #endregion
+
+    #region 方法區域
+
+    /// <summary>
+    /// 角色的跳躍:跳躍音效，往上跳
+    /// </summary>
+    private void Jump()
+    {
+        print("跳躍");
+    }
+    /// <summary>
+    /// 角色的滑行:縮小物體，
+    /// </summary>
+    private void Slide()
+    {
+        print("滑行");
+    }
+    /// <summary>
+    /// 碰撞物體：扣血
+    /// </summary>
+    private void Hit()
+    {
+
+    }
+    /// <summary>
+    /// 角色吃金幣
+    /// </summary>
+    private void Eatcoin()
+    {
+
+    }
+    /// <summary>
+    /// 死亡:結束遊戲
+    /// </summary>
+    private void Dead()
+    {
+
+    }
+
+    #endregion
+
+    #region 事件區域
+
+    private void Start()
+    {
+        Jump();
+    }
+
+    private void Update()
+    {
+        Slide();
+    }
+
+    #endregion
+}
